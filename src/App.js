e663bc9e29298f72fd9b58933236b52de48ab76d
Juan Sebastian Castro <juan.castro@grability.com>
2020-11-18 09:49:02 -0500

    ready

--- src/App.js
+++ src/App.js
@@ -1,56 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+
+import Pressure from "./features/pressure/Pressure";
+import Temperature from "./features/temperature/Temperature";
+import RainChanceChart from "./features/charts/rainChance/RainChanceChart";
+import RainfallAmount from "./features/charts/rainfallAmount/RainfallAmountChart";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+        <div className="variables-container">
+            <Temperature />
+            <Pressure />
+        </div>
+        <div className="charts-container">
+            <RainfallAmount />
+            <RainChanceChart />
+        </div>
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import pressureReducer from '../features/pressure/pressureSlice';
+import temperatureReducer from "../features/temperature/temperatureSlice";
+import rainfallReducer from "../features/charts/rainfallAmount/rainfallSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    pressure: pressureReducer,
+    temperature: temperatureReducer,
+    rainfallAmounts: rainfallReducer
   },
 });
